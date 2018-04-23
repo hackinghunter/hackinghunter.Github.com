a336aba19014356365151ca6ee61e54311143ceb
hackinghunter <37363215+hackinghunter@users.noreply.github.com>
2018-04-23 22:18:30 +0530

    Initial commit

---
+++ js/JS.js
@@ -0,0 +1,44 @@
+
+function fun()
+{
+
+	var v = document.getElementById("d").value;
+//alert("sorry! bhai kuch bar to le."+v);
+
+if(v=="")
+{
+	alert("sorry! bhai kuch bar to le.");
+}
+else
+{
+vv=v;
+		
+}
+
+
+switch(vv)
+   {
+   case "google"://password change.
+   window.open("https://www.google.com");//location.
+   break;
+    case "fb":
+   window.open("https://www.facebook.com");
+   break;
+    case "gmail":
+   window.open("https://www.gmail.com");
+   break;
+    case "youtube":
+   window.open("https://www.youtube.com");
+   break;
+   default:
+   
+   }
+/*
+function fun(){
+	var v = document.getElementById("d").value;
+	
+}*/
+
+
+}
+
